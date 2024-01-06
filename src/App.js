17cb1329b1553541820b3a8b1378c9fed253ac9c
IshaKumari10 <isha02740@gmail.com>
2024-01-06 23:34:23 +0530

    Till 43:17

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from "./MyComponents/Header"
+import Header from "./MyComponents/Header";
+import Todos from "./MyComponents/Todos";
+import Footer from "./MyComponents/Footer";
+
+
 function App() {
   return (
     <>
-     <Header></Header>
+     <Header/>
+     <Todos/>
+     <Footer/>
     </>
   );
 }
