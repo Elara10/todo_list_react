9cd01e0d777e1f83d98459b6a5f95af926eccb37
IshaKumari10 <isha02740@gmail.com>
2024-01-14 13:40:01 +0530

    completed

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -13,10 +13,10 @@ const Todos = (props) => {
      <h3 className="my-3">Todos List</h3>
      {props.todos.length===0?"No Todos to display":
      props.todos.map((todo)=>{
+      console.log(todo.sno);
       return(
-      <>
       <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}/>
-      </> 
+       
       )
      
      })
